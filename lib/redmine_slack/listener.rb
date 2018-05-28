f068368b3fa1cb5ddff9dfdbdd54a61b5dd32b53
Tomi Pakarinen <tomi.pakarinen@resys.io>
2018-05-28 14:42:29 +0300

    fix webhook post

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -143,7 +143,7 @@ class SlackListener < Redmine::Hook::Listener
 			client = HTTPClient.new
 			client.ssl_config.cert_store.set_default_paths
 			client.ssl_config.ssl_version = :auto
-			client.post_async url, {:payload => params.to_json}
+			client.post_async url, {:body => params.to_json, :header => {'Content-Type' => 'application/json'}}
 		rescue Exception => e
 			Rails.logger.warn("cannot connect to #{url}")
 			Rails.logger.warn(e)
