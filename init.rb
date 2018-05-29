96ace264592ce14f91ea45777ac2d1b9add5538e
Tomi Pakarinen <tomi.pakarinen@resys.io>
2018-05-29 15:44:58 +0300

    renaming slack

--- init.rb
+++ init.rb
@@ -1,22 +1,20 @@
 require 'redmine'
 
-require_dependency 'redmine_slack/listener'
+require_dependency 'redmine_hangouts_chat/listener'
 
-Redmine::Plugin.register :redmine_slack do
-	name 'Redmine Slack'
+Redmine::Plugin.register :redmine_hangouts_chat do
+	name 'Redmine Hangouts Chat'
 	author 'Samuel Cormier-Iijima'
-	url 'https://github.com/sciyoshi/redmine-slack'
-	author_url 'http://www.sciyoshi.com'
-	description 'Slack chat integration'
+	url 'https://github.com/patope/redmine-hangouts-chat'
+	description 'Google Hangouts Chat integration'
 	version '0.2'
 
 	requires_redmine :version_or_higher => '0.8.0'
 
 	settings \
 		:default => {
-			'callback_url' => 'http://slack.com/callback/',
+			'callback_url' => 'https://chat.googleapis.com/v1/',
 			'channel' => nil,
-			'icon' => 'https://raw.github.com/sciyoshi/redmine-slack/gh-pages/icon.png',
 			'username' => 'redmine',
 			'display_watchers' => 'no'
 		},
